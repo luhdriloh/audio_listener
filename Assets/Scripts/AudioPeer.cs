9e4ee5ab3e4272bde3e74cdd5379a1f2ef05e25c
luhdriloh <007.sam.moore@gmail.com>
2018-11-15 16:13:50 -0700

    finished tut

--- Assets/Scripts/AudioPeer.cs
+++ Assets/Scripts/AudioPeer.cs
@@ -2,24 +2,103 @@
 
 public class AudioPeer : MonoBehaviour
 {
-    public static float[] _samples = new float[512];
-    public static float[] _bands = new float[8];
+    public static float[] _samplesLeft = new float[512];
+    public static float[] _samplesRight = new float[512];
+    public static float[] _frequencyBands = new float[8];
+    public static float[] _frequencyBandBuffer = new float[8];
+    private float[] _bufferDecrease = new float[8];
+
+    public float highestFrequencyValue;
+    public float[] _maxFrequencies = new float[8];
+    public static float[] _clampedFrequencyBands = new float[8];
+    public static float[] _clampedFrequncyBandsBuffer = new float[8];
+
+    public static float _amplitude, _amplitudeBuffer, _highestAmplitude;
+
+
     private AudioSource _audioSource;
 
     private void Start()
     {
         _audioSource = GetComponent<AudioSource>();
+        AudioProfile(highestFrequencyValue);
     }
 
     private void Update()
     {
         GetSpectrumAudioSource();
         MakeFrequencyBands();
+        BandBuffer();
+        CreateAudioBands();
+        GetAmplitude();
     }
 
     private void GetSpectrumAudioSource()
     {
-        _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+        _audioSource.GetSpectrumData(_samplesLeft, 0, FFTWindow.Blackman);
+        _audioSource.GetSpectrumData(_samplesRight, 1, FFTWindow.Blackman);
+    }
+
+    private void AudioProfile(float highestFrequency)
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            _maxFrequencies[i] = highestFrequency;
+        }
+    }
+
+    private void BandBuffer()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            if (_frequencyBands[i] > _frequencyBandBuffer[i])
+            {
+                _frequencyBandBuffer[i] = _frequencyBands[i];
+                _bufferDecrease[i] = .0005f;
+            }
+            else if (_frequencyBands[i] < _frequencyBandBuffer[i])
+            {
+                float newValue = _frequencyBandBuffer[i] - _bufferDecrease[i];
+                newValue = newValue < 0 ? 0 : newValue;
+
+                _frequencyBandBuffer[i] = newValue;
+                _bufferDecrease[i] *= 1.2f;
+            }
+        }
+    }
+
+    private void CreateAudioBands()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            if (_frequencyBands[i] > _maxFrequencies[i])
+            {
+                _maxFrequencies[i] = _frequencyBands[i];
+            }
+
+            _clampedFrequencyBands[i] = _frequencyBands[i] / _maxFrequencies[i];
+            _clampedFrequncyBandsBuffer[i] = _frequencyBandBuffer[i] / _maxFrequencies[i];
+        }
+    }
+
+    private void GetAmplitude()
+    {
+        float currentAmplitude = 0f;
+        float currentAmplitudeBuffer = 0f;
+
+        for (int i = 0; i < 8; i++)
+        {
+            currentAmplitude += _clampedFrequencyBands[i];
+            currentAmplitudeBuffer += _clampedFrequncyBandsBuffer[i];
+        }
+
+        if (currentAmplitude > _highestAmplitude)
+        {
+            _highestAmplitude = currentAmplitude;
+        }
+
+        _amplitude = currentAmplitude / _highestAmplitude;
+        _amplitudeBuffer = currentAmplitudeBuffer / _highestAmplitude;
     }
 
     private void MakeFrequencyBands()
@@ -34,8 +113,6 @@ public class AudioPeer : MonoBehaviour
          * 6000 - 20000 hertz
          */
 
-        // use powers of 2 to go up
-
         int count = 0;
 
         for (int i = 0; i < 8; i++)
@@ -45,12 +122,14 @@ public class AudioPeer : MonoBehaviour
 
             for (int j = 0; j < samplesToRead; j++)
             {
-                average += _samples[count];
+                average += _samplesLeft[count] + _samplesRight[count];
                 count++;
             }
 
             average /= samplesToRead;
-            _bands[i] = average;
+
+            // find the frequency jump and the new band value average
+            _frequencyBands[i] = average;
         }
     }
 }
--- Assets/Scripts/CreateBands.cs
+++ Assets/Scripts/CreateBands.cs
@@ -4,23 +4,59 @@ using UnityEngine;
 public class CreateBands : MonoBehaviour
 {
     public GameObject _cubePrototype;
+    public float _baseScale;
+    public float _maxScale;
+    public bool _useBuffer;
 
     private GameObject[] _bandCubes;
+    private GameObject[] _frequencyJumpCubes;
 
     private void Start()
     {
+        // set up the frequency band cubes
         _bandCubes = new GameObject[8];
 
         for (int i = 0; i < 8; i++)
         {
             GameObject newCube = Instantiate(_cubePrototype);
             newCube.transform.position = transform.position;
+            newCube.transform.parent = transform;
 
+            newCube.transform.position = Vector3.right * (float)((i - 3.5) * 2);
+            _bandCubes[i] = newCube;
+        }
+
+        // set up the frequency jump cubes
+        // need to find a threshold to create the frequency jumps at
+        _frequencyJumpCubes = new GameObject[8];
+        
+        for (int i = 0; i < 8; i++)
+        {
+            GameObject newCube = Instantiate(_cubePrototype);
+            newCube.transform.position = transform.position;
+            newCube.transform.parent = transform;
+
+            newCube.transform.position = Vector3.right * (float)((i - 3.5) * 2) + Vector3.forward * -20;
+            _frequencyJumpCubes[i] = newCube;
         }
     }
 
     private void Update()
     {
+        for (int i = 0; i < 8; i++)
+        {
+            float bandValue;
+            if (_useBuffer)
+            {
+                bandValue = AudioPeer._frequencyBandBuffer[i];
+            }
+            else
+            {
+                bandValue = AudioPeer._frequencyBands[i];
+            }
 
+            _bandCubes[i].transform.localScale = new Vector3(_baseScale, (bandValue * _maxScale) + 1, _baseScale);
+            //_frequencyJumpCubes[i].transform.localScale = new Vector3(_baseScale, (AudioPeer._jumps[i] * _maxScale) + 1, _baseScale);
+        }
     }
 }
